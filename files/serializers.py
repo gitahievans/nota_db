4a48b2805169098ca0ae33451fa5a760e5d31e96
Evans Gitahi <evanygk@gmail.com>
2024-11-09 06:44:42 +0300

    add custom update method for categories in the serializer

--- files/serializers.py
+++ files/serializers.py
@@ -31,6 +31,25 @@ class PDFFileSerializer(serializers.ModelSerializer):
 
         return pdf_file
 
+    def update(self, instance, validated_data):
+        categories_data = validated_data.pop("categories", None)
+
+        # Update PDFFile fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Update categories if provided
+        if categories_data is not None:
+            # Clear existing categories
+            instance.categories.clear()
+            for category_data in categories_data:
+                category_name = category_data.get("name")
+                category, _ = Category.objects.get_or_create(name=category_name)
+                instance.categories.add(category)
+
+        return instance
+
     def to_internal_value(self, data):
         categories_data = data.get("categories", [])
         if categories_data:
