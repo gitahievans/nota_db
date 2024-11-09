3867c2ba093bf4408588faeafd201c35b774d317
Evans Gitahi <evanygk@gmail.com>
2024-11-09 03:13:36 +0300

    add category \serializer

--- files/serializers.py
+++ files/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
-from .models import PDFFile
+from .models import PDFFile, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
 
 class PDFFileSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+
     class Meta:
         model = PDFFile
-        fields = ['id', 'title', 'lyrics', 'uploaded_at', 'pdf_file', 'composer', 'year', 'categories']
-        
+        fields = "__all__"
+
     def validate_file(self, value):
-        if value.content_type != 'application/pdf':
+        if value.content_type != "application/pdf":
             raise serializers.ValidationError("Only PDF files are allowed.")
         return value
