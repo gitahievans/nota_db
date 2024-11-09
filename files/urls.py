2b9ee5661ac11da4c6893c6e075d6f0356dead63
Evans Gitahi <evanygk@gmail.com>
2024-11-09 06:21:43 +0300

    update path for update function

--- files/urls.py
+++ files/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PDFUploadView, PDFListView, PDFDownloadView, CategoryListView
+from .views import PDFUploadView, PDFListView, PDFDownloadView, CategoryListView, UpdatePDFView
 
 urlpatterns = [
     path('files/', PDFListView.as_view(), name='pdf-list'),
     path('upload/', PDFUploadView.as_view(), name='pdf-upload'),
     path('download/<int:pk>/download', PDFDownloadView.as_view(), name='pdf-download'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('update/<int:pk>/', PDFUploadView.as_view(), name='pdf-update'),
+    path('update/<int:pk>/', UpdatePDFView.as_view(), name='pdf-update'),
 ]
