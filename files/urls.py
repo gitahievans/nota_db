c382e2597e5a7e8781b684dcd62ef54d0a09c614
Evans Gitahi <evanygk@gmail.com>
2025-06-17 19:55:32 +0300

    things working both on backend and frontend

--- files/urls.py
+++ files/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
-from .views import PDFUploadView, PDFListView, PDFDownloadView, CategoryListView, UpdatePDFView
+from .views import (
+    PDFUploadView,
+    PDFListView,
+    PDFDownloadView,
+    CategoryListView,
+    UpdatePDFView,
+    PDFFileDetailView,
+    ServeMIDIView,
+    ServeMusicXMLView,
+)
 
 urlpatterns = [
-    path('files/', PDFListView.as_view(), name='pdf-list'),
-    path('upload/', PDFUploadView.as_view(), name='pdf-upload'),
-    path('download/<int:pk>/download', PDFDownloadView.as_view(), name='pdf-download'),
-    path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('update/<int:pk>/', UpdatePDFView.as_view(), name='pdf-update'),
+    path("files/", PDFListView.as_view(), name="pdf-list"),
+    path("upload/", PDFUploadView.as_view(), name="pdf-upload"),
+    path("download/<int:pk>/download", PDFDownloadView.as_view(), name="pdf-download"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("update/<int:pk>/", UpdatePDFView.as_view(), name="pdf-update"),
+    path("scores/<int:pk>/", PDFFileDetailView.as_view(), name="pdf-detail"),
+    path(
+        "serve-musicxml/<int:score_id>/",
+        ServeMusicXMLView.as_view(),
+        name="serve_musicxml",
+    ),
+    path("serve-midi/<int:score_id>/", ServeMIDIView.as_view(), name="serve_midi"),
 ]
